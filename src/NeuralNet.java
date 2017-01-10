9be6fd0601212cbaa7c13eb35a7e1c531b93a583
Dmitry <tuxarb777@yandex.ru>
2017-01-10 21:15:05 +0300

    add class NeuralNet

---
+++ src/NeuralNet.java
@@ -0,0 +1,48 @@
+/**
+ * Created by Dmitry.
+ */
+
+public class NeuralNet {
+    private double inputX;
+    private int layer1Count, layer2Count;
+
+    private double teachCoeff;
+
+    private int maxCicleCount;
+
+    public NeuralNet() {
+    }
+
+    public void setCount(int layer1Count, int layer2Count){
+
+    }
+
+    public void setInput(double input){
+
+    }
+
+    public double calculate()
+    {
+        return 0;
+    }
+
+    public void setRandomWeights(double a, double b){
+
+    }
+
+    public void teach(double input, double trueAnswer){
+
+    }
+
+    public void setTeachCoeff(double newTC){
+
+    }
+
+    public double getInputX() {
+        return inputX;
+    }
+
+    public void setInputX(double inputX) {
+        this.inputX = inputX;
+    }
+}

b867df8abb1cf84cf8d5687b0ca6d36597fe6c5f
ph4rr3ll <pharrell@yandex.ru>
2017-01-11 00:09:24 +0300

    Another step

--- src/app/Source.java
+++ src/app/Source.java
@@ -2,6 +2,8 @@ package app;
 
 import app.net.NeuralNet;
 
+import java.util.Random;
+
 public class Source {
 
     public static void main(String[] args) {
@@ -9,6 +11,22 @@ public class Source {
         net.setCount(10, 1);
         net.setRandomWeights(0.01, 0.1);
         net.setTeachCoeff(0.0001);
+
+        double x;
+        Random random = new Random();
+        for (int i = 1; i < 100000; i ++) {
+            x = random.nextDouble() % 100000 + 1;
+            x /= 10000;
+
+            // TODO: определиться со структурой точки (x1 x2)
+//            net.Teach(x, f(x));
+//            Net.setInput(x);
+//            qDebug() << i << " " << x << " " << Net.calculate() << " " << f(x) - Net.calculate();
+        }
+    }
+
+    public static double f(double x1, double x2) {
+        return 0; // TODO: определить функцию 2-х переменных
     }
 
 }

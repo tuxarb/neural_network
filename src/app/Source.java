b2e17595b10b41e8ef2864e9bdbfde2b7a91b354
ph4rr3ll <pharrell@yandex.ru>
2017-01-10 23:57:09 +0300

    Begin to work at Source class

--- src/app/Source.java
+++ src/app/Source.java
@@ -1,7 +1,14 @@
 package app;
 
-public class Source {
-    public static void main(String[] args) {
+import app.net.NeuralNet;
 
+public class Source {
+
+    public static void main(String[] args) {
+        NeuralNet net = new NeuralNet();
+        net.setCount(10, 1);
+        net.setRandomWeights(0.01, 0.1);
+        net.setTeachCoeff(0.0001);
     }
+
 }
